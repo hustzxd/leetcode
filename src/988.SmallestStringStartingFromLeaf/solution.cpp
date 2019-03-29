424a29084db5df955fc56ea0428c3efc9e87fe2e
hustzxd <2227820226@qq.com>
2019-03-29 22:09:18 +0800

    update

---
+++ src/988.SmallestStringStartingFromLeaf/solution.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <sstream>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+
+class Solution {
+public:
+    string smallestFromLeaf(TreeNode* root) {
+        // cout << string(1, 'z' + 1) << endl; "{"
+        string res = help(root, "");
+        return res;
+    }
+private:
+    string help(TreeNode* root, string tail) {
+        if (root == NULL) return "{";
+        string s = string(1, 'a' + root->val);
+        if (root->left == NULL && root->right == NULL) return s;
+        string left = help(root->left, s + tail) + s;
+        string right = help(root->right, s + tail) + s;
+        // cout << "left: " << left + tail << " right: " << right + tail << endl;
+        if (left + tail < right + tail) return left;
+        else return right;        
+    }
+};
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        string ret = Solution().smallestFromLeaf(root);
+
+        string out = (ret);
+        cout << out << endl;
+    }
+    return 0;
+}
